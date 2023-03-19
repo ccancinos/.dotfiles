0563e7e16a964a4335b344848ad30d6f2720058d
ccancinos <ccancinos@users.sourceforge.net>
2023-03-19 19:57:35 -0300

    Update install.sh

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 git clone --bare https://github.com/ccancinos/.dotfiles.git $HOME/.dotfiles
-alias config='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+function config() {
+  git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
 
 config checkout
 if [ $? = 0 ]; then
@@ -9,7 +11,7 @@ if [ $? = 0 ]; then
   else
     echo "Backing up pre-existing dotfiles.";
     mkdir -p .dotfiles-backup
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} sh -c 'mkdir -p .dotfiles-backup/{}; mv {} .dotfiles-backup/{};'
 fi;
 config checkout
 config config --local status.showUntrackedFiles no
